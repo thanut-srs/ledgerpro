22d06d4d3315c5c1b903e998f3eb266aacf3b6c1
Nuttaphon <nattaphon@sirisoft.co.th>
2019-08-01 18:59:23 +0700

    [TEST] : [Add transaction] : add some test

---
+++ src/pages/create-wallet/create-wallet.spec.ts
@@ -0,0 +1,66 @@
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { SqlProvider } from '../../providers/sql/sql';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CreateWalletPage } from './create-wallet';
+import { IonicModule, ViewController, NavController } from 'ionic-angular';
+import { SqlProviderMock } from '../../mocks/sqlprovider.mocks'
+//let component = undefined;
+
+describe('create wallet page', () => {
+  let component: CreateWalletPage;
+  let fixture: ComponentFixture<CreateWalletPage>;
+  let de: DebugElement;
+
+  let viewCtrlSpy = jasmine.createSpyObj('ViewController',
+    ['data', 'readReady', 'writeReady', 'dismiss', '_setHeader', '_setNavbar', '_setIONContent', '_setIONContentRef'])
+  viewCtrlSpy['readReady'] = {
+    subscribe() { }
+  }
+  viewCtrlSpy.writeReady = {
+    subscribe() { }
+  }
+
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreateWalletPage],
+      imports: [IonicModule.forRoot(CreateWalletPage)],
+      providers: [{ provide: ViewController, useValue: viewCtrlSpy },
+      { provide: SqlProvider, useValue: SqlProviderMock },
+        FormBuilder,
+        NavController],
+
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateWalletPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component instanceof CreateWalletPage).toBeTruthy()
+  });
+
+  it('form should invalid when there no value in formfield', () => {
+
+  })
+
+  it('form should valid when there is value in formfield', () => {
+
+  })
+
+  it('submit form should call createwallet', () => {
+
+  })
+
+
+
+
+
+});

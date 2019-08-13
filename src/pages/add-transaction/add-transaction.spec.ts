50dffb10ae3f38ab316fc131b0de703943868146
Nuttaphon <nattaphon@sirisoft.co.th>
2019-08-13 17:30:16 +0700

    [TEST] : fixed some test

--- src/pages/add-transaction/add-transaction.spec.ts
+++ src/pages/add-transaction/add-transaction.spec.ts
@@ -14,12 +14,12 @@ describe('Add Trasaction page', () => {
   let de: DebugElement;
 
   let viewCtrlSpy = jasmine.createSpyObj('ViewController',
-                          ['data', 'readReady', 'writeReady', 'dismiss', '_setHeader', '_setNavbar', '_setIONContent', '_setIONContentRef'])
+    ['data', 'readReady', 'writeReady', 'dismiss', '_setHeader', '_setNavbar', '_setIONContent', '_setIONContentRef'])
   viewCtrlSpy['readReady'] = {
-    subscribe(){}
+    subscribe() { }
   }
   viewCtrlSpy.writeReady = {
-    subscribe(){}
+    subscribe() { }
   }
 
 
@@ -27,10 +27,9 @@ describe('Add Trasaction page', () => {
     TestBed.configureTestingModule({
       declarations: [AddTransactionPage],
       imports: [IonicModule.forRoot(AddTransactionPage)],
-      providers: [{ provide : ViewController, useValue: viewCtrlSpy },
-        { provide: SqlProvider, useValue: SqlProviderMock },
+      providers: [{ provide: ViewController, useValue: viewCtrlSpy },
+      { provide: SqlProvider, useValue: SqlProviderMock },
         FormBuilder],
-
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
       .compileComponents();
@@ -46,7 +45,7 @@ describe('Add Trasaction page', () => {
     expect(component instanceof AddTransactionPage).toBeTruthy()
   });
 
-  it('should have form',() => {
+  it('should have form', () => {
     let form = fixture.debugElement.query(By.css('#transactionForm'));
     expect(form).toBeDefined()
   })
@@ -55,13 +54,13 @@ describe('Add Trasaction page', () => {
     expect(Object.keys(component.transaction.controls).length).toEqual(6)
   })
 
-  it('should have add transaction button',() => {
+  it('should have add transaction button', () => {
     let btnsubmit = fixture.debugElement.query(By.css('#submitbtn'));
-    console.log('submit======',btnsubmit)
+    console.log('submit======', btnsubmit)
     expect(btnsubmit.name).toEqual('button')
   })
 
-  it('input amount should not disabled', () => {
+  it('inputamount field should not disabled', () => {
     let inputamt = fixture.debugElement.query(By.css('#amountinput'));
     expect(inputamt.children[0].properties.disabled).toBeFalsy()
   })
@@ -70,65 +69,39 @@ describe('Add Trasaction page', () => {
     expect(component.transaction.valid).toBeFalsy()
   })
 
-  it('type should have no value when enter app' ,() => {
+  it('type should have no value when enter app', () => {
     expect(component.transaction.controls['type'].value).toEqual('')
   })
-  it('tag should have no value when enter app' ,() => {
+  it('tag should have no value when enter app', () => {
     expect(component.transaction.controls['tag'].value).toEqual('')
   })
 
-  it('walletname should have no value when enter app' ,() => {
+  it('walletname should have no value when enter app', () => {
     expect(component.transaction.controls['walletName'].value).toEqual('')
   })
 
-  it('amount should have no value when enter app' ,() => {
+  it('amount should have no value when enter app', () => {
     expect(component.transaction.controls['amount'].value).toEqual('')
   })
-  it('memo should have no value when enter app' ,() => {
+  it('memo should have no value when enter app', () => {
     expect(component.transaction.controls['memo'].value).toEqual('')
   })
 
-  it('memo should not have validator' ,() => {
-    // expect(component.transaction.controls['memo'].validator).toEqual('')
-    //console.log('mmeeemememllllll;;;;;',component.transaction.controls['memo'])
-    //console.log('amoutntntntntn;;;;;',component.transaction.controls['amount'])
+  it('memo should not have validator', () => {
     expect(component.transaction.controls['memo'].validator).toBeNull()
   })
-  it('amount should required' ,() => {
+  it('amount should required', () => {
     let errors = component.transaction.controls['amount'].errors
     expect(errors['required']).toBeTruthy()
     component.transaction.controls['amount'].setValue('')
     expect(component.transaction.controls['amount'].valid).toBeFalsy()
   })
 
-  it('date should have no value when enter app' ,() => {
+  it('component.currentDate should have no value when enter app', () => {
     expect(component.transaction.controls['date'].value).toEqual(null)
   })
 
-  // it('button should invalid when there no value in formfield', () => {
-  //   let btnsubmit = fixture.debugElement.nativeElement.querySelector('#submitbtn').disabled;
-  //   expect(btnsubmit).toBeTruthy()
-  // })
-
-  // it('button should valid when there value in formfield', () => {
-  //   let datepick = fixture.debugElement.query(By.css('#datepick'));
-  //   let typeselect = fixture.debugElement.query(By.css('#typeselect'));
-  //   let tagselect = fixture.debugElement.query(By.css('#tagselect'));
-  //   let goalselect = fixture.debugElement.query(By.css('#goalselect'));
-  //   let amountinput = fixture.debugElement.query(By.css('#amountinput'));
-  //   datepick.nativeElement.value = '2019-07-22'
-  //   typeselect.nativeElement.value = 'Income'
-  //   tagselect.nativeElement.value = 'Food'
-  //   amountinput.nativeElement.value = 30
-  //   setFormTruthValue();
-  //   let btnsubmitDisable = fixture.debugElement.nativeElement.querySelector('#submitbtn').disabled;
-  //   fixture.detectChanges()
-  //   console.log('amtinput::',amountinput.nativeElement)
-  //   expect(amountinput.nativeElement).toEqual( component.transaction.controls['amount'].value)
-  //   expect(btnsubmitDisable).toBeFalsy()
-  // })
-
-  const setFormTruthValue = function() {
+  const setFormTruthValue = function () {
     component.transaction.controls['type'].setValue('Income')
     component.transaction.controls['tag'].setValue('Food')
     component.transaction.controls['amount'].setValue(30)
@@ -151,20 +124,19 @@ describe('Add Trasaction page', () => {
     let btnsubmit = fixture.debugElement.query(By.css('#submitbtn'))
     setFormTruthValue();
     fixture.detectChanges()
-    console.log('---- ---- btnsubmit after fill form ;;;; ', btnsubmit)
     expect(btnsubmit.properties['disabled']).toBeFalsy()
   })
 
-  it('submit form should call onInsertTable', () => {
-    spyOn(component,"onInsertTable")
+  it('submit form should call function onInsertTable', () => {
+    spyOn(component, "onInsertTable")
     let formsubmit = fixture.debugElement.query(By.css('#transactionForm'));
     formsubmit.triggerEventHandler('ngSubmit', null);
     expect(component.onInsertTable).toHaveBeenCalledTimes(1);
   })
 
-  it('set date should set come date',() => {
+  it('function setDate should set component.currentDate', () => {
     component.setDate()
-    expect(this.currentDate).not.toBeNull
+    expect(component.currentDate).not.toBeNull
   })
 
 
